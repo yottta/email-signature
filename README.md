81711c7072e078481dacc060f19c228cb73545be
yottta <andreic9203@gmail.com>
2022-10-21 12:59:33 +0300

    feature(script): add generate script and readme file

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # email-signature
 Email Signature template
+
+## Usage
+Just run `./generate.sh` and follow instructions.
+In case you have no ways to execute the script, just open [index.html](index.html) and replace the following:
+* `[NAME]` - your name
+* `[TITLE]` - your title
+* `[PHONE]` - your phone number
+* `[SITE_URL]` - the url of your website
+* `[SITE]` - the name of the website
+* `[LINKED_IN_URL]` - your linkedin profile url
+* `[GITHUB_URL]` - your github profile
+
+### Modify
+In case you need different social links, just edit [index.html](index.html) section at the end where you can find the links and the icons.
+
+
+## License
+
+This repo is available under the [MIT license](https://github.com/yottta/email-signature/blob/main/LICENSE).
--- generate.sh
+++ generate.sh
@@ -3,15 +3,20 @@
 read -p "Enter your name:" name
 read -p "Enter your title:" title
 read -p "Enter your phone number:" phone_number
+read -p "Enter your web site URL:" site_url
 read -p "Enter your LinkedIn profile URL:" linkedin
 read -p "Enter your Github profile URL:" github
+
+site=`echo $site_url | sed -e "s~http://~~" -e "s~https://~~" -e "s~/~~g"`
 
 echo "Name: $name" 
 echo "Title: $title"
 echo "Phone Number: $phone_number"
+echo "Personal site URL: $site_url"
+echo "Site name: $site"
 echo "LinkedIn: $linkedin"
 echo "Github: $github"
 
 read -p "Verifiy the information and hit enter to continue generation" confirmation
 
-sed -i "" -e "s/\[NAME\]/$name/g" -e "s/\[TITLE\]/$title/g" -e "s/\[PHONE\]/$phone/g" -e "s~\[LINKED_IN_URL\]~$linkedin~g" -e "s~\[GITHUB_URL\]~$github~g" index.html
+sed -i "" -e "s~\[NAME\]~$name~g" -e "s~\[TITLE\]~$title~g" -e "s~\[PHONE\]~$phone_number~g" -e "s~\[SITE_URL\]~$site_url~g" -e "s~\[SITE\]~$site~g" -e "s~\[LINKED_IN_URL\]~$linkedin~g" -e "s~\[GITHUB_URL\]~$github~g" index.html
